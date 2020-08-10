30628da0998517bd103b799901e3789f42fcc404
Brett Sherwood <clockmakerbrett@gmail.com>
2020-08-10 08:55:12 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <body>
+          <nav>
+            <img src="./images/ironhack-logo.svg" alt="logo" />
+            <img src="./images/menu-top.svg" alt="logo" />
+          </nav>
+          <main>
+            <h1>Say Hello to React.js</h1>
+            <p>You will learn a frontend framework from scratch to become a Ninja developer</p>
+            <div id="link-div">
+              <a href="#">Awesome!</a>
+            </div>
+          </main>
+        </body>
       </div>
     );
   }
